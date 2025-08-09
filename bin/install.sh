3902356d687c13ac02a18df8f9447a46b95803db
Andres Parra <byandrev@gmail.com>
2025-08-09 16:53:12 -0500

    add command to install flatpak

--- bin/install.sh
+++ bin/install.sh
@@ -72,3 +72,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Flatpak
+sudo pacman -S flatpak
